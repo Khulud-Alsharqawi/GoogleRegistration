026ed8a77c21d4307d0a5debce74e305bb6c1708
khulud <Khulud.Saleh.ruh.k@tuwaiq.edu.sa>
2021-11-05 00:11:46 +0300

    update-> still working on check box ;.;

--- app/src/main/java/com/example/googleregistration/MainActivity.kt
+++ app/src/main/java/com/example/googleregistration/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.googleregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.googleregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,33 +14,36 @@ class MainActivity : AppCompatActivity() {
         binding.next.setOnClickListener { registration() }
     }
 
-    fun registration() {
-        val firstName = binding.firstName.text.toString()
-        val lastName = binding.lastName.text.toString()
-        val email = binding.useEmail.text.toString()
-        val password = binding.password.text.toString()
-        val confirm = binding.confirm.text.toString()
+
+    private fun registration() {
+        val firstName = binding.firstNameInput.text.toString()
+        val lastName = binding.lastNameInput.text.toString()
+        val email = binding.userNameInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        val confirm = binding.confirmInput.text.toString()
 
         if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirm.isNotEmpty()) {
-            if (email.contains('!') || email.contains('@') || email.contains(' ') || email.contains(
-                    '#'
-                )
-                || email.contains('$') || email.contains('%') || email.contains('^')
-                || email.contains('&') || email.contains('*') || email.contains('?')
-                || email.contains('>') || email.contains('-') || email.contains('_')
+            if (email.contains('!') || email.contains('@')||email.contains(' ') || email.contains('#')
+                || email.contains('$')||email.contains('%') || email.contains('^')
+                || email.contains('&')||email.contains('*') || email.contains('?')
+                || email.contains('>')||email.contains('-') || email.contains('_') ){
+              //  binding.result.setText("Please Enter User Name Without Symbol or spaces")
+                Toast.makeText(this, "Please Enter User Name Without Symbol or spaces", Toast.LENGTH_SHORT).show()
 
-
-            ) {
-                binding.result.setText("Please Enter User Name Without Symbol or spaces")
             }
-            if (password == confirm) {
-                binding.result.setText("Hi $firstName you register successfully")
+           else if (password == confirm  ) {
+                binding.result.setText("Hi ${firstName}@gmail.com you register successfully")
+
             } else {
-                binding.result.setText("Password Missmatch")
+                Toast.makeText(this, "Password Missmatch", Toast.LENGTH_SHORT).show()
+                 //   binding.result.setText("Password Missmatch")
             }
 
         } else {
-            binding.result.setText("Please full all blanks")
+            Toast.makeText(this, "Please full all blanks", Toast.LENGTH_SHORT).show()
+
         }
+
     }
 }
+
